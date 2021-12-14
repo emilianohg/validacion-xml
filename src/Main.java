65d00a83e3578ac22589e01845858dacd7a5ae05
emilianohg <emiliano4144@hotmail.com>
2021-12-14 14:21:56 -0700

    procesarDOM

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
@@ -44,8 +45,43 @@ public class Main {
             System.out.println(node.item(0).getNodeName());
             System.out.println(node.item(0).getTextContent());
 
+            Node root = doc.getElementsByTagName("blog").item(0);
+            procesarDOM(root);
+
         } catch (ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    // TODO: revisar y si pueden mejorarlo
+    public static void procesarDOM(Node nodo) {
+        switch (nodo.getNodeType()) {
+            case Node.DOCUMENT_NODE:
+                System.out.println("<%xml version=\"1.0\">");
+                Document doc = (Document) nodo;
+                procesarDOM(doc.getDocumentElement());
+                break;
+            case Node.ELEMENT_NODE:
+                String nombre=nodo.getNodeName();
+                System.out.println("<"+nombre+">");
+                if(nodo.getChildNodes() != null) {
+                    for(int i=0 ; i<nodo.getChildNodes().getLength() ; i++) {
+                        for(int j=0 ; j< nodo.getAttributes().getLength() ; j++) {
+                            procesarDOM(nodo.getAttributes().item(j));
+                        }
+                        procesarDOM(nodo.getChildNodes().item(i));
+                    }
+                }
+                System.out.println("</"+nombre+">");
+                break;
+            case Node.ATTRIBUTE_NODE:
+                System.out.println(""+nodo.getNodeName()+"=\""+nodo.getNodeValue()+"\"");
+
+                break;
+            case Node.TEXT_NODE://"\n? *\n?"
+                if (!nodo.getTextContent().matches("\n? *\n?"))
+                    System.out.println(nodo.getTextContent());
         }
 
     }
